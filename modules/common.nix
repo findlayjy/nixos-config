9ff435688d3c26263e40a0ff23549a47e61d103c
Jamie Findlay <jy.findlay@gmail.com>
2026-06-20 23:58:38 +0200

    Split settings between common.nix and machine-specific configuration.nix

--- modules/common.nix
+++ modules/common.nix
@@ -1,4 +1,65 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
+  # Packages
+  programs.firefox.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
+    alacritty
+    git
+    ripgrep
+    zsh
+  ];
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # Timezone and locale
+  time.timeZone = "Europe/Oslo";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "uk";
+
+  # Desktop
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Audio
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Printing
+  services.printing.enable = true;
+
+  # User
+  users.users.jamief = {
+    isNormalUser = true;
+    description = "Jamie Findlay";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Nix settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  nix.settings.auto-optimise-store = true;
 }
