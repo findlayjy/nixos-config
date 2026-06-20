bf4e5dc680b0bc71d3402aeeb3a353e03aae802e
Jamie Findlay <jy.findlay@gmail.com>
2026-06-21 01:16:47 +0200

    Added dotfiles to symlink

--- home.nix
+++ home.nix
@@ -4,4 +4,14 @@
   home.username = jamief;
   home.homeDirectory = /home/jamief;
   home.stateVersion = 25.11;
+
+  home.file = {
+    ".zshrc".source = ./dotfiles/.zshrc;
+    ".zshenv".source = ./dotfiles/.zshenv;
+    ".zsh".source = ./dotfiles/.zsh;
+    ".vimrc".source = ./dotfiles/.vimrc;
+    ".latexmkrc".source = ./dotfiles/.latexmkrc;
+    ".dir_colors".source = ./dotfiles/.dir_colors;
+    ".config/alacritty".source = ./dotfiles/.config/alacritty;
+  };
 }
