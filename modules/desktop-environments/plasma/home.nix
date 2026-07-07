910f1dd24dbec7c3e385d536746cc67d0ed002bf
Jamie Findlay <jy.findlay@gmail.com>
2026-07-07 22:44:04 +0200

    added nord theme to plasma

--- modules/desktop-environments/plasma/home.nix
+++ modules/desktop-environments/plasma/home.nix
@@ -3,13 +3,30 @@
 {pkgs, ...}:
 
 {
+
+	home.packages = with pkgs; [
+		nordic
+		nordzy-cursor-theme
+		papirus-icon-theme
+	];
+
+	# Declarative KDE control via plasma-manager
+	programs.plasma = {
+    	enable = true;
+    
+	    workspace = {
+	      lookAndFeel = "Nordic";
+	      colorScheme = "Nordic";
+	      theme = "Nordic";
+	    };
+	  }; 
 # GTK
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Nordic";
-      package = pkgs.nordic;
-    };
-  gtk4.theme = null; # Adopt new default behaviour
-  };
+ # gtk = {
+  #  enable = true;
+   # theme = {
+    #  name = "Nordic";
+     # package = pkgs.nordic;
+#    };
+ # gtk4.theme = null; # Adopt new default behaviour
+  #};
 }
