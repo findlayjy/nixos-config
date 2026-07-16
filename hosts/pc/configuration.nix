6209fa8d510cafadb9dbb5e79e76aef9b0ccbc8e
Jamie Findlay <jy.findlay@gmail.com>
2026-07-16 15:40:18 +0200

    Adding pc host

---
+++ hosts/pc/configuration.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/common.nix
+  ];
+
+  # Boot settings
+  # Here, use systemd-boot to pick a disk from the BIOS boot menu, since Pop!_OS is installed on a different drive. Could remove and switch to grub once I'm single booting.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 3; # seconds
+
+  networking.hostName = "pc";
+
+  # system.stateVersion = "25.11"; # Fill in with install version
+}
---
+++ hosts/pc/home.nix
@@ -0,0 +1,5 @@
+{ config, pkgs, ... }:
+
+{
+  # home.stateVersion = "25.11"; # Home Manager first-install version
+}
