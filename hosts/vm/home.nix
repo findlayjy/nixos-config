d72bd7c38f302871213157780fe96fdca70c0f32
Jamie Findlay <jy.findlay@gmail.com>
2026-07-16 15:40:32 +0200

    Moved home.stateVersion to vm home.nix

--- hosts/vm/home.nix
+++ hosts/vm/home.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  home.stateVersion = "25.11"; # Home Manager first-install version
+
   # Enable Spice vdagent so clipboard works both ways
   home.file.".config/autostart/spice-vdagent.desktop".text = ''
     [Desktop Entry]
