380e74837df73713d73180d6ea479748fb87fdc8
Jamie Findlay <jy.findlay@gmail.com>
2026-07-20 22:34:42 +0200

    Adding nvidia driver settings and steam for pc

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -1,9 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  networking.hostName = "pc";
   imports = [
     ./hardware-configuration.nix
     ../../modules/common.nix
+    ../../modules/steam.nix
   ];
 
   # Boot settings
@@ -12,7 +14,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 3; # seconds
 
-  networking.hostName = "pc";
+  # NVidia settings
+  services.xserver.videoDrivers = [ "nvidia" ];
+  #
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;  # proprietary driver rather than open-source kernel module needed for newer cards
+    nvidiaSettings = true; # GUI settings tool
+    package = config.boot.kernelPackages.nvidiaPackages.stable; # stable rather than beta/production branch
+  };
 
   system.stateVersion = "26.05";
 }
