0222a63ef8a6597285da8d21c8a27b583ee66812
Jamie Findlay <jy.findlay@gmail.com>
2026-07-23 21:19:23 +0200

    switching to grub bootloader

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -9,8 +9,13 @@
   ];
 
   # Boot settings
-  # Here, use systemd-boot to pick a disk from the BIOS boot menu, since Pop!_OS is installed on a different drive. Could remove and switch to grub once I'm single booting.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    devices = [ "nodev" ];  # UEFI install, not writing to an MBR
+    useOSProber = true;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 3; # seconds
 
